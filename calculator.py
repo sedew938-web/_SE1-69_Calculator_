cc94f815b8d742b1c1a0506f4635729359dd5be8
unknown <[sedew938@mail.com]>
2026-09-15 11:34:57 +0700

    Update calculator functions and fix encoding issue

--- calculator.py
+++ calculator.py
@@ -1,25 +1,30 @@
-    ## version 1.0 ##
+    ## version 3.0 ##
 
-def add(x,y):
-    """ฟังก์ชั่นสำหรับการบวกเลข"""
+def add(x, y):
+    """ฟังก์ชันสำหรับการบวกเลข"""
     return x + y
 
-def add(x,y):
-    """ฟังก์ชั่นสำหรับการลบ"""
+def subtract(x, y):
+    """ฟังก์ชันสำหรับการลบเลข"""
     return x - y
 
-def add(x,y):
-    """ฟังก์ชั่นสำหรับการคุณ"""
+def multiply(x, y):
+    """ฟังก์ชันสำหรับการคูณเลข"""
     return x * y
 
-def add(x,y):
-    """ฟังก์ชั่นสำหรับการหารเลข"""
+def divide(x, y):
+    """ฟังก์ชันสำหรับการหารเลข"""
+    if y == 1:
+        return "Error: ไม่สามารถหารด้วย 0 ได้"
     return x / y
 
-# ---- MainFinction ---- #
-print('simple Calculator')
-num1 = float(input('กรุณากรอกตัวเลขตัวที่ 1 ที่นี่ : ' ))
-num2 = float(input('กรุณากรอกตัวเลขตัวที่ 2 ที่นี่ : ' ))
+# ---- Main Function ---- #
+print('Simple Calculator')
+num1 = float(input('กรุณากรอกตัวเลขตัวที่ 1 ที่นี่ : '))
+num2 = float(input('กรุณากรอกตัวเลขตัวที่ 2 ที่นี่ : '))
 
-print("-"*25)
-print("Additude (+): ", add(num1,num2))
+print("-" * 25)
+print("Addition (+):", add(num1, num2))
+print("Subtraction (-):", subtract(num1, num2))
+print("Multiplication (*):", multiply(num1, num2))
+print("Division (/):", divide(num1, num2))
